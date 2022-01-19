7609eaf79916d702f60b8b30aea62b02ea1eca3a
MJ Aguirre <michaelaquila1998@gmail.com>
2022-01-19 20:41:31 +0800

    Finish Control Flow Activity

---
+++ L1_control_flow/divisible.rb
@@ -0,0 +1,11 @@
+arr = [6, 3, 1, 8, 4, 2, 10, 65, 102]
+
+arr.each do |number|
+    puts number % 2 == 0 ? true : false
+end
+
+# def by_two? number
+#     number % 2 == 0 ? true : false
+# end
+
+p 
