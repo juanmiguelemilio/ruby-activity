a4ddb973c498b6e9e04508c94ad4eed2dfe178d8
MJ Aguirre <michaelaquila1998@gmail.com>
2022-01-21 21:30:59 +0800

    Update divisible.rb

--- L1_control_flow/divisible.rb
+++ L1_control_flow/divisible.rb
@@ -1,5 +1,9 @@
 arr = [6, 3, 1, 8, 4, 2, 10, 65, 102]
+arr2 = []
 
-arr.each do |number|
-    puts number % 2 == 0 ? true : false
-end
+arr.each {|number|
+  if number % 2 == 0
+	arr2.push(number)
+end}
+
+p arr2
