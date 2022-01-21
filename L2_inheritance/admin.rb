6ad211fe3f735103ba7019e59ce3ce527bb0696e
MJ Aguirre <michaelaquila1998@gmail.com>
2022-01-21 22:21:52 +0800

    Update admin.rb

--- L2_inheritance/admin.rb
+++ L2_inheritance/admin.rb
@@ -68,9 +68,7 @@ class Buyer < User
   end
 
   include BuyerPermission
-  def change_password
-    puts "Password changed!"
-  end
+  include ChangePassword
 end
 
 
@@ -83,11 +81,9 @@ end
 my_admin = Admin.new('avionuser', 'password', '127.0.0.1')
 my_admin.admin_login
 my_admin.edit_users_profile
-
-my_admin.change_password = 'new_password'
+my_admin.change_password
 
 buyer = Buyer.new('juan', 'password', '127.0.0.1')
 buyer.buyer_login
 buyer.buy
-
-buyer.change_password = 'new_password'
+buyer.change_password
